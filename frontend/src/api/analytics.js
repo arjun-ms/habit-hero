f7c04d67a9d7a107fb16d4bb16d37c67e2cf92bb
arjun-ms <arjunachu123.aa@gmail.com>
2025-12-05 16:32:58 +0530

    fix(frontend): update api url

--- frontend/src/api/analytics.js
+++ frontend/src/api/analytics.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+// const API = "http://127.0.0.1:8000";
+const API = import.meta.env.VITE_API_URL;
 
 export const getHabitAnalytics = async (habitId) => {
   const res = await axios.get(`${API}/habits/${habitId}/analytics`);
